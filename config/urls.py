d451b495a6da5a0dd9b0e72ffc16e952c1e72027
nightwind <nw4869@gmail.com>
2017-11-03 02:21:25 +0800

    update url for vue

--- config/urls.py
+++ config/urls.py
@@ -9,14 +9,18 @@ from . import api_urls
 
 
 urlpatterns = [
-    url(r'^home/$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
-    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+    ################## for vue #################
+    url(r'^.*$', TemplateView.as_view(template_name="index.html")),
+
+    ################## for django #################
+    # url(r'^home/$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    # url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
 
     # User management
-    url(r'^users/', include('sagittarius.users.urls', namespace='users')),
+    # url(r'^users/', include('sagittarius.users.urls', namespace='users')),
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
